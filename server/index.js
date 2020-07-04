525089427eb28c173f81db77bbdb9c1dddc513b5
juser0719 <sinsa0719@naver.com>
2020-07-04 19:50:43 +0900

    setting-and-login-css

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+const router = require('./router');
+const app = express();
+const server =http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('New Connection!');
+    socket.on('disconnect', () => {
+        console.log('User had left :( ');
+    });
+});
+
+app.use(router);
+server.listen(PORT, () => console.log(`server started, port : ${PORT}  `));
